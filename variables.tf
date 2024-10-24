e10ea9a6d8e65a24d9b9ff66b9933fc6e8362eec
Atishay <atishay2024@outlook.com>
2024-10-24 15:05:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,61 +1,59 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
+
 variable "region" {
-  description = "The AWS region to deploy to"
-  type        = string
-  default     = "eu-north-1" # Default value can be changed
-}
-
-variable "vpc_id" {
-  description = "The ID of the existing VPC"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "The ID of the existing subnet"
+  description = "AWS Region"
   type        = string
 }
 
 variable "ami_id" {
-  description = "The AMI ID to use for the instance"
+  description = "Amazon Machine Image ID"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
   type        = string
 }
 
 variable "instance_type" {
-  description = "The type of instance to create"
+  description = "EC2 Instance Type"
   type        = string
-  default     = "t2.micro" # Default instance type
 }
 
 variable "key_pair_name" {
-  description = "The name of the key pair to use for the instance"
+  description = "Key pair name"
   type        = string
-}
-
-variable "ingress_cidr_blocks" {
-  description = "CIDR blocks for ingress rules"
-  type        = list(string)
-  default     = ["0.0.0.0/0"] # Open to the world (adjust as necessary)
 }
 
 variable "security_group_name" {
-  description = "Name of the security group"
+  description = "Security group name"
   type        = string
-  default     = "allow-ssh"
 }
 
 variable "volume_size" {
-  description = "Size of the root volume in GB"
+  description = "Root volume size (in GB)"
   type        = number
-  default     = 20 # Default size can be changed
 }
 
 variable "volume_type" {
-  description = "Type of the root volume"
+  description = "Root volume type"
   type        = string
-  default     = "gp2" # General Purpose SSD
 }
 
 variable "instance_name" {
   description = "Name tag for the EC2 instance"
   type        = string
-  default     = "MyEC2Instance"
 }
